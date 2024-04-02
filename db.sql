9ab80759258e434fea289f1168f77f31499abf35
lasha-codes <lashagiorgi420@gmail.com>
2024-04-02 15:27:53 -0700

    learning more about Altering a table cause i know all of this kinda

--- db.sql
+++ db.sql
@@ -14,5 +14,17 @@ CREATE TABLE test (
 -- how to work with dates
 INSERT INTO test VALUES (CURRENT_DATE(), CURRENT_TIME(), NOW());
 INSERT INTO test VALUES (CURRENT_DATE() + 1, NULL, NULL); -- + 1 on CURRENT_DATE function adds one day to it
+DROP TABLE test; -- bye bye
 
 SELECT * FROM test;
+
+-- i know this but still working on UNIQUE data types
+CREATE TABLE products (
+  product_id INT,
+  product_name VARCHAR(25) UNIQUE -- ensures that product_name is unique in the database,
+  price DECIMAL(4, 2),
+);
+
+-- let's say we forgot to add our constraint a column.
+ALTER TABLE products ADD CONSTRAINT UNIQUE(product_name);
+SELECT * FROM products;
